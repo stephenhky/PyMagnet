e320906a61e5317f07fa05a7f210f4d7c34f51fd
stephenhky <stephenhky@yahoo.com.hk>
2016-10-17 16:17:21 -0400

    testing gradient descent

--- FieldPolarizedMagnet.py
+++ FieldPolarizedMagnet.py
@@ -2,23 +2,19 @@ import numpy as np
 import theano
 import theano.tensor as T
 
+# good reference: http://www.marekrei.com/blog/theano-tutorial/
+
 # defining
 r, u = T.dscalars('r', 'u')
 m, H = T.dvectors('m', 'H')
-H = 0.5*r*T.dot(m, m) + 0.25*u*T.dot(m, m)*T.dot(m, m) - T.dot(H, m)   # Landau model
+helmoltz_energy = 0.5*r*T.dot(m, m) + 0.25*u*T.dot(m, m)*T.dot(m, m) - T.dot(H, m)   # Landau model
 
 # numerical variables
-grad_m = T.grad(H, m)
+grad_m = T.grad(helmoltz_energy, m)
 alpha = T.dscalar('alpha')
 init_m = T.sqrt(T.abs_(r)/u)
 
 # Helhholtz free energy
-energy_density = theano.function(inputs=[r, u, H, m], outputs=H)
+energy_density = theano.function(inputs=[r, u, H, m], outputs=helmoltz_energy)
+grad_energy_wrt_m = theano.function(inputs=[r, u, H, m], outputs=grad_m)
 
-# gradient descent
-gresults, gupdates = theano.scan(lambda oldm: oldm - alpha*grad_m,
-                                 outputs_info={
-
-                                     T.ones_like(init_m)
-                                 },
-                                 non_sequences=m)
